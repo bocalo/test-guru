d6314db9cabebb669ffde700af5ba29f153ec33f
bocalo <moldavskiym@yandex.ru>
2021-07-15 10:13:56 +0300

    Add new migration

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,14 +7,17 @@
 #  title       :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  author_id   :integer
 #  category_id :integer
 #
 # Indexes
 #
+#  index_tests_on_author_id    (author_id)
 #  index_tests_on_category_id  (category_id)
 #
 # Foreign Keys
 #
+#  author_id    (author_id => users.id)
 #  category_id  (category_id => categories.id)
 #
 class Test < ApplicationRecord
@@ -22,7 +25,7 @@ class Test < ApplicationRecord
   has_many :test_users
   has_many :users, through: :test_users
   belongs_to :category
-  belongs_to :author, class_name: "User", foreign_key: 'user_id'
+  belongs_to :author, class_name: "User", foreign_key: 'author_id'
 
   def self.sort_tests_by_title(category_title)
     Test.joins('JOIN categories ON categories.id = tests.category_id')
--- db/schema.rb
+++ db/schema.rb
@@ -8,9 +8,9 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It's strongly recommended that you check this file into your version control system!
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_101011) do
+ActiveRecord::Schema.define(version: 2021_07_15_052458) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_07_12_101011) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -64,4 +66,5 @@ ActiveRecord::Schema.define(version: 2021_07_12_101011) do
   add_foreign_key "test_users", "tests"
   add_foreign_key "test_users", "users"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
