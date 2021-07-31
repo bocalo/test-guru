03b4e2e5ea87613c3d15186c52fdcbfb8b460ac8
bocalo <moldavskiym@yandex.ru>
2021-07-31 18:38:45 +0300

    Add new files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  
+  root 'tests#index'
+
+  resources :answers
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,22 +1,3 @@
-# == Schema Information
-#
-# Table name: answers
-#
-#  id          :integer          not null, primary key
-#  body        :text             not null
-#  correct     :boolean          default(FALSE)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  question_id :integer
-#
-# Indexes
-#
-#  index_answers_on_question_id  (question_id)
-#
-# Foreign Keys
-#
-#  question_id  (question_id => questions.id)
-#
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
