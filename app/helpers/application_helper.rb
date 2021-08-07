4c7f5c0975466e5f637308e0a5c6ea79e3e055ed
bocalo <moldavskiym@yandex.ru>
2021-08-07 11:21:01 +0300

    Ninth task

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo, project_name)
     link_to "#{project_name}", "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message
+    content_tag :p, flash[:alert], class: 'flash alert' if flash[:alert]
+  end
 end
