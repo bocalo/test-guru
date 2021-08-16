0e7c31d964017db017bc5872b1ab9187dd4b3c21
bocalo <moldavskiym@yandex.ru>
2021-08-16 17:43:52 +0300

    Eleventh task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
   def after_sign_in_path_for(user)
     user.admin? ? admin_tests_path : tests_path
@@ -7,5 +8,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User < ApplicationRecord
   end
 
   def admin?
-    is_a(Admin)
+    is_a?(Admin)
   end
 end
 
