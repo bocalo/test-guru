073034f2765006ac091a2dd0245a26d2d2f8d878
bocalo <moldavskiym@yandex.ru>
2021-08-19 12:01:24 +0300

    Twelfth task

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,8 @@
 class TestsController < ApplicationController
-  
+  before_action :authenticate_user!
   before_action :set_test, only: %i[start]
-  
+  before_action :set_user, only: :start
+
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
@@ -17,6 +18,10 @@ class TestsController < ApplicationController
 
   def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
   end
 
   def rescue_with_test_not_found
