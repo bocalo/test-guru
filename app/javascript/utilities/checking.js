5c519b04d4d79131e9dbe4f5d275b17f95b402c4
bocalo <moldavskiym@yandex.ru>
2021-09-06 10:50:25 +0300

    Add new change

--- app/javascript/utilities/checking.js
+++ app/javascript/utilities/checking.js
@@ -10,7 +10,11 @@ function confirmPassword() {
   var password = document.getElementById("user_password");
   var control = document.getElementById("user_password_confirmation");
 
-  if (this.value == password.value) {
+  if (!this.value.length) {
+    document.querySelector(".octicon-thumbsup").classList.add("hide");
+    document.querySelector(".octicon-thumbsdown").classList.add("hide");
+    return;
+  } else if (this.value == password.value) {
     document.querySelector(".octicon-thumbsup").classList.remove("hide");
     document.querySelector(".octicon-thumbsdown").classList.add("hide");
     control.classList.add("border-green");
@@ -20,10 +24,5 @@ function confirmPassword() {
     document.querySelector(".octicon-thumbsdown").classList.remove("hide");
     control.classList.add("border-red");
     control.classList.remove("border-green");
-  }
-
-  if (!this.value.length) {
-    document.querySelector(".octicon-thumbsup").classList.add("hide");
-    document.querySelector(".octicon-thumbsdown").classList.add("hide");
   }
 }
