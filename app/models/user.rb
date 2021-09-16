223d55a66ca1416326d125b81f0597951da14e92
bocalo <moldavskiym@yandex.ru>
2021-09-16 11:37:56 +0300

    Add new code

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: :author_id
   has_many :gists
+  has_many :feedbacks
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Invalid email format' }
   validates :name, presence: true
---
+++ test/models/feedback_test.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_feedbacks_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+require 'test_helper'
+
+class FeedbackTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
