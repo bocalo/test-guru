2a6003f6a8fd38cd5db860581cf3fb96848f58e3
bocalo <moldavskiym@yandex.ru>
2021-09-18 18:01:30 +0300

    Add change again

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,7 @@
 
 users = User.create!([
   { name: 'Tom', email: 'tom@example.com', first_name: 'Tom', last_name: 'Hanks', password: 'secretus' },
-  { name: 'Adamas', email: 'adamas@example.com', first_name: 'Adamas', last_name: 'Monti', password: 'secretus', type: 'Admin' },
-  { name: 'Mikhail', email: 'm52d8c1@gmail.com', first_name: 'Mikhail', last_name: 'Moldavskiy', password: 'Qwerty123', type: 'Admin'}
+  { name: 'Adamas', email: 'adamas@example.com', first_name: 'Adamas', last_name: 'Monti', password: 'secretus', type: 'Admin' }
 ])
 
 categories = Category.create!([
